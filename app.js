01694c78815307cd675d2a7b83ccabbf9b7ed2f4
kaitlyn mockett <kaitlynm98760@gmail.com>
2021-04-24 13:59:09 +0100

    added switch light mode icon functionality, not light modes

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const todo = document.getElementById('to-do');
 const container = document.querySelector('.todo-container');
 const list = document.querySelector('.todo-list');
 const clearBtn = document.querySelector('.clear-btn');
+const numItems = document.querySelector('.items-active');
+const switchModeBtn = document.querySelector('.toggle-light-mode');
 
 // stuff that doesnt work yet
-const numItems = document.querySelector('.items-active');
 const activeBtn = document.querySelector('.active-btn');
 const completedBtn = document.querySelector('.completed-btn');
 const toggleStateBtn = document.querySelector('.toggle-state-btn');
 
+// active items counter
 let counter = 0;
+let mode = 'light';
 // Edit options - Constants
 let editElement;
 let editFlag = false;
@@ -29,7 +32,10 @@ clearBtn.addEventListener('click', clearItems);
 activeBtn.addEventListener('click', activeFilter);
 
 completedBtn.addEventListener('click', completedFilter);
+
 toggleStateBtn.addEventListener('click', completedFilter);
+
+switchModeBtn.addEventListener('click', switchMode)
 // Functions
 // add item to list
 function addItem(e) {
@@ -67,6 +73,7 @@ function addItem(e) {
         counter++;
         // add class to display container
         container.classList.add('show-container');
+        // show number of items in list active
         showActive()
         // add item to local storage
         addToLocalStorage(id, value);
@@ -94,7 +101,8 @@ function clearItems() {
     // if there are any items
     if (items.length > 0) {
         items.forEach(function (item) {
-            list.removeChild(item)
+            list.removeChild(item);
+            counter--;
         })
     }
 
@@ -119,6 +127,8 @@ function deleteItem(e) {
     const element = e.currentTarget.parentElement.parentElement;
     const id = element.dataset.id;
     list.removeChild(element);
+    counter--;
+    showActive()
     if (list.children.length === 0) {
         container.classList.remove('show-container');
     }
@@ -139,6 +149,22 @@ function showActive(){
     else {
         displayAlert('Internal Error', 'danger')
     }
+}
+
+function switchMode () {
+    if (mode === 'light') {
+        switchModeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>`;
+        mode = 'dark';
+    }
+    else if (mode = 'dark') {
+        switchModeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>`;
+        mode = 'light';
+        // change class of bg-img to show correct bg img
+    }
+    else {
+        displayAlert('error', 'danger')
+    }
+   
 }
 // toggle todo icon state // not working
 function setState () {
